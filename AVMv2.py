275c5052b8d7c522286c914ea5d53210aaf75b3a
Vittoriax1 <99565350+Vittoriax1@users.noreply.github.com>
2022-07-01 16:03:54 -0400

    Add files via upload

---
+++ AVMv2.py
@@ -0,0 +1,14 @@
+# This is version 2, created by Jennifer Souvannasing
+animal = 'Chicken'
+vegetable = 'Eggplant'
+mineral = 'Gold'
+print('-' * 50)
+name = input('Please enter your name: ')
+print('Hello, {}.'.format(name))
+print('-' * 50)
+print('Here is an animal, a vegetable, and a mineral.')
+print(animal.upper())
+print(vegetable)
+print(mineral.lower())
+print('-' * 50)
+print('Have a great day, {}.'.format(name))
